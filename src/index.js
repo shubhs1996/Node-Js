55b047bc465038ed9cbccf54a0548ffa2f79e5c7
shubhs1996 <40110783+shubhs1996@users.noreply.github.com>
2019-07-28 02:17:11 +0530

    init commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 const express=require('express')
-const path=require('path')
+
 
 const mongoose=require('mongoose')
 require('./db/mongoose')
-
-
+const hbs=require('hbs')
+const path=require('path')
 const User=require('./models/user')
 const Blog=require('./models/blog')
 const blogRouter =require('./routers/blog')
-
+const pathdir=path.join(__dirname,'../public')
+const viewpath=path.join(__dirname,'../templates/views')
 
 
 const app= express()
 const port =process.env.PORT||3000
 
 app.use(express.json())
+app.set('view engine','hbs')
+app.set('views',viewpath)
+app.use(express.static(pathdir))
 app.use(blogRouter)
 
 
 app.get('/',(req,res)=>{
-    
+ res.render('index')
 })
 
 
